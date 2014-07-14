058c5765a69c587c33a22715cb0d9feca3e1e6ba
Gazok <jamiebayne@hotmail.com>
2014-07-14 22:27:21 +0100

    Added stone cursor

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,16 +1,21 @@
 #include "Board.hpp"
 
 #include <iostream>
+#include <cmath>
 #include <Tank/System/Mouse.hpp>
 #include <Tank/Graphics/RectangleShape.hpp>
 
 Board::Board(unsigned size)
-  : grid_(tank::Vectoru{size,size})
+  : grid_(tank::Vectoru{size,size}, Empty)
   //, size_(size)
 {
     const auto pixelSize = stoneSize * size;
     auto g = makeGraphic<tank::RectangleShape>(tank::Vectoru{pixelSize,pixelSize});
     g->setFillColor({0,125,0});
+
+    const unsigned stoneRadius = std::ceil(stoneSize/2.0);
+    cursor_ = makeGraphic<tank::CircleShape>(stoneRadius);
+    cursor_->setOrigin({stoneRadius,stoneRadius});
 }
 
 void Board::onAdded()
@@ -23,10 +28,27 @@ void Board::onAdded()
 
 void Board::mouseOver()
 {
-    std::cout << "Mouse over" << std::endl;
+    using M = tank::Mouse;
+    cursor_->setPos(M::getPos());
+    //cursor_->(M::getPos());
+    isIn_ = true;
 }
 
 void Board::onClick()
 {
     std::cout << "Mouse click" << std::endl;
+}
+
+void Board::update()
+{
+    if (wasIn_ and not isIn_) {
+        hideCursor();
+    }
+    wasIn_ = isIn_;
+    isIn_ = false;
+}
+
+void Board::hideCursor()
+{
+    cursor_->setPos({-stoneSize, -stoneSize});
 }
--- src/Board.hpp
+++ src/Board.hpp
@@ -1,21 +1,28 @@
 #include <Tank/System/Entity.hpp>
-#include <Tank/Utility/CollisionGrid.hpp>
+#include <Tank/Utility/Grid.hpp>
+#include <Tank/Graphics/CircleShape.hpp>
 
 class Board : public tank::Entity
 {
+private:
     enum Stone {
         White,
         Black,
         Empty
     };
 
-    tank::CollisionGrid grid_;
+    tank::Grid<Stone> grid_;
+    tank::observing_ptr<tank::CircleShape> cursor_;
+    bool isIn_ {false}, wasIn_ {false};
+
+    void hideCursor();
     //unsigned size_;
 
 public:
-    static constexpr unsigned stoneSize = 20;
+    static constexpr unsigned stoneSize = 19;
 
     virtual void onAdded() override;
+    virtual void update() override;
     void mouseOver();
     void onClick();
     Board(unsigned size = 19);
