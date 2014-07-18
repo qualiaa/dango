7ea644a25ad118a8acc6a207ee0cddb871888c95
Gazok <jamiebayne@hotmail.com>
2014-07-18 21:09:46 +0100

    Configured UI

--- src/StartWorld.cpp
+++ src/StartWorld.cpp
@@ -1,15 +1,17 @@
 #include "StartWorld.hpp"
 
 #include <Tank/Graphics/Text.hpp>
-#include "Input.hpp"
 
 tank::Font StartWorld::titleFont;
 tank::Font StartWorld::font;
 
 StartWorld::StartWorld()
 {
+    // Load fonts
     titleFont.loadFromFile("res/Anonymous Pro.ttf");
     font.loadFromFile("res/Consola.ttf");
+
+    // Make title text
     auto title = makeEntity<tank::Entity>()->makeGraphic<tank::Text>(titleFont);
     title->setColor({255,255,255});
     title->setText("dango");
@@ -17,6 +19,20 @@ StartWorld::StartWorld()
     title->setOrigin(title->getSize() / 2);
     title->setPos(tank::Vectorf(tank::Game::window()->getSize().x / 2, 50));
 
-    makeEntity<Input>(tank::Vectorf{250,300}, tank::Vectoru{200,16}, "Hostname:");
-    makeEntity<Input>(tank::Vectorf{250,330}, tank::Vectoru{200,16}, "Port:");
+    // Make input boxes
+    hostname_ = makeEntity<Input>(tank::Vectorf{250,300},
+                                  tank::Vectoru{200,16},
+                                  "Hostname:");
+    port_ = makeEntity<Input>(tank::Vectorf{250,330},
+                              tank::Vectoru{200,16},
+                              "Port:");
+
+    // Set default text
+    hostname_->setText("127.1");
+    port_->setText("8037");
+
+    // TODO: menuIndex management
+
+    // Give focus to hostname
+    hostname_->gainFocus();
 }
--- src/StartWorld.hpp
+++ src/StartWorld.hpp
@@ -5,9 +5,16 @@
 #include <Tank/System/Game.hpp>
 #include <Tank/Graphics/Font.hpp>
 #include "MainWorld.hpp"
+#include "Input.hpp"
 
+// TODO: UI placement factory
 class StartWorld : public tank::World
 {
+    tank::observing_ptr<Input> hostname_;
+    tank::observing_ptr<Input> port_;
+
+    //unsigned menuIndex_ = 0;
+
 public:
     static tank::Font font;
     static tank::Font titleFont;
