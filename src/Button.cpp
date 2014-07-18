354ec8f1d383256887ae7a6d79372dfd096f6c34
Gazok <jamiebayne@hotmail.com>
2014-07-19 00:26:58 +0100

    Added button class

---
+++ src/Button.cpp
@@ -0,0 +1,90 @@
+#include "Button.hpp"
+
+#include <Tank/System/Keyboard.hpp>
+#include <Tank/System/Mouse.hpp>
+#include "StartWorld.hpp"
+
+Button::Button(tank::Vectorf pos,
+               tank::Vectoru size,
+               std::string label,
+               std::function<void()> callback)
+    : tank::Entity(pos)
+    , callback_ {callback}
+{
+    box_ = makeGraphic<tank::RectangleShape>(size);
+    label_ = makeGraphic<tank::Text>(StartWorld::font);
+    label_->setColor({});
+
+    box_->setOutlineColor({200,200,200});
+    setFontSize(18);
+    setLabel(label);
+}
+
+void Button::setLabel(std::string label)
+{
+    label_->setText(label);
+    label_->setPos(box_->getSize() / 2);
+    label_->setOrigin((label_->getSize() / 2) + tank::Vectorf{0, 4});
+}
+
+void Button::setFontSize(unsigned s)
+{
+    label_->setFontSize(s);
+}
+
+void Button::onAdded()
+{
+    using K = tank::Keyboard;
+    using Key = tank::Key;
+    using M = tank::Mouse;
+    using B = M::Button;
+
+    auto haveFocus = [&]() { return hasFocus_; };
+    connect(K::KeyPress(Key::Return) && haveFocus, &Button::onRelease);
+    connect(M::ButtonRelease(B::Left) && M::InEntity(*this), &Button::onRelease);
+    connect(M::ButtonPress(B::Left) && M::InEntity(*this), &Button::onClick);
+}
+
+void Button::update()
+{
+    using M = tank::Mouse;
+    if (M::isInEntity(*this)) {
+        box_->setFillColor(hover);
+    }
+    else {
+        if (hasFocus_) {
+            box_->setFillColor(focus);
+        } else {
+            box_->setFillColor(normal);
+        }
+    }
+}
+
+void Button::onClick()
+{
+    box_->setFillColor(active);
+}
+
+void Button::onRelease()
+{
+    gainFocus();
+    if (callback_) callback_();
+}
+
+void Button::gainFocus()
+{
+    if (hasFocus_) return;
+
+    hasFocus_ = true;
+    box_->setFillColor(focus);
+    box_->setOutlineThickness(1);
+}
+
+void Button::loseFocus()
+{
+    if (not hasFocus_) return;
+
+    hasFocus_ = false;;
+    box_->setFillColor(normal);
+    box_->setOutlineThickness(0);
+}
---
+++ src/Button.hpp
@@ -0,0 +1,44 @@
+#ifndef BUTTON_HPP
+#define BUTTON_HPP
+
+#include <functional>
+#include <Tank/System/Entity.hpp>
+#include <Tank/Graphics/Text.hpp>
+#include <Tank/Graphics/RectangleShape.hpp>
+
+class Button : public tank::Entity
+{
+    bool hasFocus_ {false};
+    tank::observing_ptr<tank::RectangleShape> box_;
+    tank::observing_ptr<tank::Text> label_;
+    std::function<void()> callback_;
+
+    tank::Color normal {150,150,150};
+    tank::Color focus {100,100,100};
+    tank::Color hover {200,200,200};
+    tank::Color active {255,255,255};
+
+public:
+    virtual void onAdded() override;
+    virtual void update() override;
+    //virtual void draw(tank::Camera const&) override;
+
+    void setLabel(std::string label);
+    std::string getLabel() const { return label_->getText(); }
+    void setFontSize(unsigned);
+    unsigned getFontSize() const { return label_->getFontSize(); }
+
+    void onRelease();
+    void onClick();
+
+    void gainFocus();
+    void loseFocus();
+    bool hasFocus() const { return hasFocus_; }
+
+    Button(tank::Vectorf,
+           tank::Vectoru size,
+           std::string label = "",
+           std::function<void()> callback = []{});
+};
+
+#endif /* BUTTON_HPP */
