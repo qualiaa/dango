6716e88f324338d0bd1ccea389f7a9cf7444c88a
Gazok <jamiebayne@hotmail.com>
2014-07-25 00:55:29 +0100

    Changed window background color

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main()
     using namespace tank;
     Game::initialize({640,480});
     Game::window()->setCaption("dango");
+    Game::window()->setBackgroundColor({0,29,0});
     Game::makeWorld<StartWorld>();
     Game::run();
 }
